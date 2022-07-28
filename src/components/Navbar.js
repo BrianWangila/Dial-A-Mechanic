6192854797e2d21b177b689db1ea1298d41bfae6
Brian Wangila <bwanjalah@gmail.com>
2022-07-28 03:29:28 +0300

    add search bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,6 +35,12 @@ function Navbar(){
             </ul>
           </div>
         </div>
+        <div class="container-fluid">
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
       </nav>
 
       <div className="container-fluid">
