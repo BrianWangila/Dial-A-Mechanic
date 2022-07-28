3e4332b2c546e6cfd781db78a69ef9d2eecf372f
Brian Wangila <bwanjalah@gmail.com>
2022-07-28 12:13:30 +0300

    edit footer and navbar, add image on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,31 +4,39 @@ import React from "react";
 function Footer (){
 
   return(
-  <nav className="navbar sticky-bottom bg-light" style={{height: 30+"vh", marginTop: 2+"vh"}}>
+  <nav className="" style={{height: 30+"vh", marginTop: 2+"vh"}}>
     <h5><span>DAM</span> Dial A Mechanic</h5>
-    <div className="footer-flex">
+    <div className="flexbox">
       <div>
-        <h5>Contact Us</h5>
-        <ul>
-          <li>Facebook</li>
-          <li>Instagram</li>
-          <li>Twitter</li>
-          <li>LinkedIn</li>
-        </ul>
+        <p>Physical Location</p>
+        <img src="https://www.bankrate.com/2021/08/09163309/Roadside-assistance-vs-AAA.jpg?auto=webp&optimize=high&crop=16:9"/>
       </div>
       <div>
-        <p>Design by Brian</p>
-      </div>
-      <div>
-        <h5>About Us</h5>
-        <ul>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Website Policies</li>
-          <li>Privacy Policy</li>
-        </ul>
+        <div>
+          <h5>Contact Us</h5>
+          <ul>
+            <li>Facebook</li>
+            <li>Instagram</li>
+            <li>Twitter</li>
+            <li>LinkedIn</li>
+          </ul>
+        </div>
+
+        <div>
+          <h5>About Us</h5>
+          <ul>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Website Policies</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
       </div>
     </div>
+
+    <center>
+        <p>Design by Brian</p>
+    </center>
   </nav>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar(){
       <nav>
         <div class="nav nav-tabs" id="nav-tab" role="tablist">
           <li class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">
-            <Link to="/" className="nav-link" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</Link>
+            <Link to="/" className="nav-link" >Home</Link>
           </li>
           <li className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">
             <Link to="/our-work" className="nav-link">Our Work</Link>
@@ -20,50 +20,21 @@ function Navbar(){
           <li className="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">
             <Link to="/about-us" className="nav-link">About Us</Link>
           </li>
-          <li className="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">
+          <li className="nav-link dropdown" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">
             <Link to="/search" className="nav-link">Search</Link>
           </li>
-
-          <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="">
-              Are you a Mechanic ?
-            </button>
-            <ul className="dropdown-menu">
-              <li className="nav-link">
-                <Link to="/dam/login" className="nav-link">Create Account</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="container-fluid">
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
         </div>
       </nav>
       
-      
+
 
       <div className="container-fluid">
         <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/our-work" className="nav-link">Our Work</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about-us" className="nav-link">About Us</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/search" className="nav-link">Search</Link>
-          </li>
           <div className="dropdown">
             <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="">
               Are you a Mechanic ?
             </button>
-            <ul className="dropdown-menu">
+            <ul className="dropdown-menu">          
               <li className="nav-item">
                 <Link to="/dam/login" className="nav-link">Create Account</Link>
               </li>
