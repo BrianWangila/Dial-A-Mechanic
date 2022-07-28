94d06f3c8873f8859554713ffa3b4ad9ed91665c
Brian Wangila <bwanjalah@gmail.com>
2022-07-28 22:48:17 +0300

    edit the about us page and style

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,53 +5,41 @@ export default function AboutUs(){
 
   return(
     <div>
-      <center><h1>About Us</h1></center>
+      <center style={{marginTop: 10+"vh"}}><h3>About Us</h3></center>
       <div className="aboutPage">
-        <div>
-          <div>
-            <h5>We Look out for you and your car:</h5>
-            <p>At DAM, we value both you and the car. While we strive to provide the best and affordable maintenance services, we also
-              strive to ensure that you are comfortable and do not encounter more problems in the future.
-            </p>
+          <div class="card" style={{width: 48+"vw"}}>
+            <div class="card-body">
+              <h5 class="card-title">We Look out for you and your car:</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Because you matter to us</h6>
+              <p class="card-text">At DAM, we value both you and the car. While we strive to provide the best and affordable maintenance services, we also
+                strive to ensure that you are comfortable and do not encounter more problems in the future.</p>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">We Look out for you and your car:</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Because you matter to us</h6>
+              <p class="card-text">At DAM, we value both you and the car. While we strive to provide the best and affordable maintenance services, we also
+                strive to ensure that you are comfortable and do not encounter more problems in the future.</p>
+            </div>
           </div>
-          <div>
-            <h5>We Look out for you and your car:</h5>
-            <p>At DAM, we value both you and the car. While we strive to provide the best and affordable maintenance services, we also
-              strive to ensure that you are comfortable and do not encounter more problems in the future.
-            </p>
-          </div>
-          <div>
-            <h5>We Look out for you and your car:</h5>
-            <p>At DAM, we value both you and the car. While we strive to provide the best and affordable maintenance services, we also
-              strive to ensure that you are comfortable and do not encounter more problems in the future.
-            </p>
-          </div>
-        </div>
         <img src="https://newsroom.aaa.com/wp-content/uploads/2012/12/AAA-Experts-Made-the-List-Now-Check-it-Twice-to-Avoid-Being-One-of-the-1.2-Million-Motorists-Stranded-this-Holiday-Season.jpg"/>
       </div>
 
       <div className="aboutPage">
         <img src="https://newsroom.aaa.com/wp-content/uploads/2012/12/AAA-Experts-Made-the-List-Now-Check-it-Twice-to-Avoid-Being-One-of-the-1.2-Million-Motorists-Stranded-this-Holiday-Season.jpg"/>
-        <div>
-          <div>
-            <h5>We Look out for you and your car:</h5>
-            <p>At DAM, we value both you and the car. While we strive to provide the best and affordable maintenance services, we also
-              strive to ensure that you are comfortable and do not encounter more problems in the future.
-            </p>
+        <div class="card" style={{width: 48+"vw"}}>
+            <div class="card-body">
+              <h5 class="card-title">We Look out for you and your car:</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Because you matter to us</h6>
+              <p class="card-text">At DAM, we value both you and the car. While we strive to provide the best and affordable maintenance services, we also
+                strive to ensure that you are comfortable and do not encounter more problems in the future.</p>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">We Look out for you and your car:</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Because you matter to us</h6>
+              <p class="card-text">At DAM, we value both you and the car. While we strive to provide the best and affordable maintenance services, we also
+                strive to ensure that you are comfortable and do not encounter more problems in the future.</p>
+            </div>
           </div>
-          <div>
-            <h5>We Look out for you and your car:</h5>
-            <p>At DAM, we value both you and the car. While we strive to provide the best and affordable maintenance services, we also
-              strive to ensure that you are comfortable and do not encounter more problems in the future.
-            </p>
-          </div>
-          <div>
-            <h5>We Look out for you and your car:</h5>
-            <p>At DAM, we value both you and the car. While we strive to provide the best and affordable maintenance services, we also
-              strive to ensure that you are comfortable and do not encounter more problems in the future.
-            </p>
-          </div>
-        </div>
       </div>
     </div>
   )
