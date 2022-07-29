d7922424b22fc7053aff50df062744437e0797f0
Brian Wangila <bwanjalah@gmail.com>
2022-07-29 08:20:54 +0300

    add our work section

--- src/components/OurWork.js
+++ src/components/OurWork.js
@@ -3,10 +3,52 @@ import React from "react";
 export default function OurWork(){
 
   return (
-    <center>
-      <h1>Our Work</h1>
+    <>
+      <center style={{marginTop: 10+"vh"}}><h3>Our Services</h3></center>
       
-    </center>
+      <div className="services">
+        <div style={{marginBottom: 5+"vh"}}>
+        <h2><li>Automotive Repair Estimates</li></h2>
+        <h4>Get cost-effective, quality auto repair from our certified experts</h4>
+        <p>At Christian Brothers Automotive, our goal is to become your partner for all of your automotive care. Whether you’re visiting one of our nationwide locations for a simple oil change, or you come to us because you aren’t sure what’s wrong with your vehicle, our automotive technicians and service team members will be by your side throughout the life of your car or truck.</p>
+
+        <h5>Why We’re Different</h5>
+        <p>While you may have experienced uncomfortable automotive repair shops in the past, with technicians who’ve tried to upsell you parts and service you don’t need, you can rest assured that at Christian Brothers Automotive, we’re different. We are committed to the Nice Difference®, which means we will never recommend unnecessary parts or service, and we will never do anything to your vehicle without your consent. Our team members will walk you through every diagnosis, repair, and part cost with transparent explanations as to why they’re needed. This allows you to be in control to make informed decisions about your automotive repair.</p>
+
+        <h5>Our Repair Estimate Process</h5>
+        <p>When you bring your car or truck to us for a diagnosis, our goal is the same as yours: to get you back on the road again as safely and quickly as possible. That’s why our automotive technicians are equipped with a variety of manufacturer diagnostic tools. We don’t want you to have to wait around for the right equipment before getting answers.</p>
+
+        <p>Once we have an understanding of what is wrong with your vehicle, we don’t leave you in the dark. Our experienced service team members will thoroughly and transparently explain the entire situation to you–from problem to price–so that you understand our service recommendation(s). At Christian Brothers Automotive, we don’t seek out the quickest or cheapest solution. We will always recommend the most thorough and safe repair options, and not just a patch job for symptoms.</p>
+        </div>
+        <div>
+          <h2><li>Oil And Filter Changes</li></h2>
+          <h4>OIl changes near you</h4>
+          <p>Your vehicle is one of the biggest and most important investments that you’ll make. Most people are constantly looking for ways to prolong the life of their car or truck, to ensure they have reliable transportation to get where they need to go. But did you know that the easiest, most effective way to preserve the life of your vehicle without spending a ton of money is to get your oil and oil filter changed regularly?</p>
+
+          <p>Many people are guilty of putting off oil changes because they don’t seem like a big deal, but clean oil and oil filters are essential in maintaining a healthy engine. Oil lubricates the multitude of moving parts within your engine, allowing it to run smoothly. Over time, oil breaks down and becomes gummy or watery, and doesn’t work as well. If left unchanged, old oil can cause expensive and inconvenient engine problems.</p>
+
+          <h5>How Regular Oil Changes Benefit Your Vehicle:</h5>
+
+          <p>Oil helps lubricate and cool the various parts of your engine, while reducing friction, diminishing wear, and forming a seal between the pistons, rings, and cylinder walls. Without the cleaning power of new oil, carbon and particle buildup become toxic to the engine. This is why we recommend that you follow the interval as suggested by your most recent oil change (the earlier of the two limits from the sticker on your windshield). Getting regular oil and filter changes is a small price to pay to avoid expensive engine repair or breakdown.</p>
+        </div>
+
+        <div>
+          <h2><li>Wheel Alignment and rotation</li></h2>
+        </div>
+
+        <div>
+          <h2><li>Checking and inspecting brake pedals</li></h2>
+        </div>
+
+        <div>
+          <h2><li>Electrical systems</li></h2>
+        </div>
+
+        <div>
+          <h2><li>Pure purchase inspections</li></h2>
+        </div>
+      </div>
+    </>
   )
 }
 
