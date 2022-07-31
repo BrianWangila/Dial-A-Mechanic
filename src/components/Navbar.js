6b78e266641037fe4fb0996b06f1cfd61e85b0ec
Brian Wangila <bwanjalah@gmail.com>
2022-07-31 15:44:40 +0300

    change position of create account button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,19 +17,21 @@ function Navbar(){
           <li className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">
             <Link to="/" className="nav-link" >Home</Link>
           </li>
-          <li className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">
+          <li className="nav-link"  data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">
             <Link to="/work" className="nav-link">Our Services</Link>
           </li>
-          <li className="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">
+          <li className="nav-link"  data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">
             <Link to="/about" className="nav-link">About Us</Link>
           </li>
-          <li className="nav-link dropdown" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">
+          <li className="nav-link" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">
             <Link to="/search" className="nav-link">Contact Us</Link>
           </li>
-        </div>
-      </nav>
+          <li className="nav-link create-account" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">
+                {/* <Link to="/dam/login" className="nav-link">Login</Link> */}
+                <Link to="/mechanic/create-account" className="nav-link">Create Account</Link>
+          </li>
 
-      <div className="container-fluid search-left">
+          {/* <div className="container-fluid">
         <ul className="navbar-nav">
           <div className="dropdown">
             <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="">
@@ -38,11 +40,17 @@ function Navbar(){
             <ul className="dropdown-menu">          
               <li className="nav-item">
                 {/* <Link to="/dam/login" className="nav-link">Login</Link> */}
-                <Link to="/mechanic/create-account" className="nav-link">Create Account</Link>
+                {/* <Link to="/mechanic/create-account" className="nav-link">Create Account</Link>
               </li>
             </ul>
           </div>
         </ul>
+      </div> */} 
+        </div>
+      </nav>
+
+      <div className="container-fluid search-left">
+        
       </div>
     </nav>
 
